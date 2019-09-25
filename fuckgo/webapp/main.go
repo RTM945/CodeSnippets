dacce5fb9a71048b05f4bf9e477cc17184fa7e13
genghuacheng <genghuacheng@bilibili.com>
2019-09-25 18:22:12 +0800

    fuck go webapp

--- fuckgo/webapp/main.go
+++ fuckgo/webapp/main.go
@@ -2,14 +2,48 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
 )
 
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+var path = "./resource/"
+
+func (p *Page) save() error {
+	filename := path + p.Title + ".txt"
+	return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := path + title + ".txt"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+	_, _ = fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/view/"):]
+	p, _ := loadPage(title)
+	_, _ = fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
 }
 
 func main() {
+	//p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
+	//p1.save()
+	//p2, _ := loadPage("TestPage")
+	//fmt.Println(string(p2.Body))
+
 	http.HandleFunc("/", handler)
-	http.ListenAndServe(":9090", nil)
+	http.HandleFunc("/view/", viewHandler)
+	_ = http.ListenAndServe(":9090", nil)
 }
