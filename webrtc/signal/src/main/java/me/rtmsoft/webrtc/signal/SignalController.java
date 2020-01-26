9e7eb00e38f2c5e4e4fc79235f2350978fae346b
RTM <assiadamo@gmail.com>
2020-01-27 00:14:35 +0800

    both offer and answer use web socket

--- webrtc/signal/src/main/java/me/rtmsoft/webrtc/signal/SignalController.java
+++ webrtc/signal/src/main/java/me/rtmsoft/webrtc/signal/SignalController.java
@@ -1,114 +1,28 @@
 package me.rtmsoft.webrtc.signal;
 
-import me.rtmsoft.webrtc.signal.vo.PairVO;
-import me.rtmsoft.webrtc.signal.vo.SdpVO;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.messaging.Message;
-import org.springframework.messaging.core.MessagePostProcessor;
-import org.springframework.messaging.handler.annotation.DestinationVariable;
-import org.springframework.messaging.handler.annotation.MessageExceptionHandler;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.messaging.simp.annotation.SendToUser;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.servlet.http.HttpSession;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.TimeUnit;
+import java.security.Principal;
 
 @Controller
 public class SignalController {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(SignalController.class);
-
-    Map<String, Answer> answers = new ConcurrentHashMap<>();
+    @Autowired
+    private PeerManager peerManager;
 
     @Autowired
     private SimpMessagingTemplate messagingTemplate;
 
-    @Autowired
-    private OfferManager offerManager;
-
-    //answer端注册
-    @MessageMapping("/register/{token}")
-    public void register(@DestinationVariable String token, Answer answer) throws Exception {
-        if(answer == null || StringUtils.isEmpty(answer.getName())) {
-            throw new Exception("name can't be empty");
+    @MessageMapping("/sdp")
+    public void sdp(@Payload String sdp, Principal principal) {
+        Peer peer = (Peer) principal;
+        Peer pair = peerManager.pair(peer);
+        if(pair != null) {
+            messagingTemplate.convertAndSendToUser(pair.getName(), "/queue/onsdp", sdp);
         }
-        if (answers.get(answer.getName()) != null) {
-            throw new Exception("duplicate name");
-        }else{
-            answer.setToken(token);
-            answers.put(answer.getName(), answer);
-            LOGGER.info("register {}", answer);
-        }
-    }
-
-    @MessageMapping("/answer")
-    public void answer(@Payload String sdp, Answer answer) throws Exception {
-        if (!StringUtils.isEmpty(sdp)) {
-            offerManager.onAnswer(answer.getName(), sdp);
-        }
-    }
-
-    @MessageExceptionHandler
-    @SendToUser("/queue/errors")
-    public String handleException(Throwable exception) {
-        return exception.getMessage();
-    }
-
-    //将offer和answer配对
-    @RequestMapping(path = "/pair", method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
-    public ResponseEntity<String> pair(@RequestBody PairVO pairVO, HttpSession session) {
-        Answer answer = answers.get(pairVO.getName());
-        if (answer != null && answer.getToken().equals(pairVO.getToken())) {
-            //配对成功写入session
-            session.setAttribute("user", pairVO.getName());
-            LOGGER.info("pair {}", answer);
-            return ResponseEntity.status(HttpStatus.OK).build();
-        }
-        return ResponseEntity.status(HttpStatus.I_AM_A_TEAPOT).build();
-    }
-
-    @RequestMapping(path = "/offer", method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
-    @ResponseBody
-    public ResponseEntity<String> offer(@RequestBody SdpVO sdpVO, HttpSession session) {
-        String user = (String) session.getAttribute("user");
-        String answer = "";
-        HttpStatus code = HttpStatus.OK;
-        if (StringUtils.isEmpty(user)) {
-            code = HttpStatus.UNAUTHORIZED;
-        } else {
-            //发送给answer
-            Offer offer = new Offer(user);
-            offerManager.add(offer);
-            LOGGER.info("offer {}", offer.getUser());
-            messagingTemplate.convertAndSendToUser(user, "/queue/offer", sdpVO);
-            //等待answer的sdp
-            try{
-                answer = offer.getAnswer(2, TimeUnit.SECONDS);
-                if (StringUtils.isEmpty(answer)) {
-                    code = HttpStatus.SERVICE_UNAVAILABLE;
-                }
-            } catch (Exception e) {
-                code = HttpStatus.BAD_REQUEST;
-            }
-        }
-
-        if (StringUtils.isEmpty(user)) {
-            code = HttpStatus.UNAUTHORIZED;
-        }
-        return ResponseEntity.status(code).body(answer);
     }
 }
--- webrtc/signal/src/test/java/me/rtmsoft/webrtc/signal/SignalApplicationTests.java
+++ webrtc/signal/src/test/java/me/rtmsoft/webrtc/signal/SignalApplicationTests.java
@@ -1,19 +1,12 @@
 package me.rtmsoft.webrtc.signal;
 
-import me.rtmsoft.webrtc.signal.vo.PairVO;
-import me.rtmsoft.webrtc.signal.vo.SdpVO;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.messaging.converter.StringMessageConverter;
 import org.springframework.messaging.simp.stomp.StompFrameHandler;
 import org.springframework.messaging.simp.stomp.StompHeaders;
 import org.springframework.messaging.simp.stomp.StompSession;
@@ -26,111 +19,52 @@ import org.springframework.web.socket.sockjs.client.SockJsClient;
 import org.springframework.web.socket.sockjs.client.WebSocketTransport;
 
 import java.lang.reflect.Type;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.Collections;
+import java.util.UUID;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class SignalApplicationTests {
+public class SignalApplicationTests {
 
-    private Logger logger = LoggerFactory.getLogger(SignalApplicationTests.class);
-
-    @Autowired
-    private TestRestTemplate restTemplate;
+    private static final Logger LOGGER = LoggerFactory.getLogger(SignalApplicationTests.class);
 
     @LocalServerPort
     int port;
 
     @Test
-    void contextLoads() {
+    void testConnection() throws Exception {
+        String baseUrl = "ws://localhost:" + port + "/signalling";
+        String token = UUID.randomUUID().toString();
+        StompSession answer = createPeer(baseUrl, token, "answer");
+        StompSession offer = createPeer(baseUrl, token, "offer");
+        offer.send("/app/sdp", "offer's sdp");
+        TimeUnit.SECONDS.sleep(1);
     }
 
-    String user = "test";
-    String token = "test_token";
-    String offer = "test_offer";
-    String answer = "test_answer";
-
-    String wsOnErrors = "/user/queue/errors";
-    String wsOffer = "/user/queue/offer";
-    String wsRegister = "/app/register";
-
-    @Test
-    void testAll() throws Exception {
-        //1.register
-        String baseUrl = "ws://localhost:" + port + "/signalling";
+    StompSession createPeer(String baseUrl, String token, String type) throws Exception {
         WebSocketStompClient stompClient = new WebSocketStompClient(new SockJsClient(Collections.singletonList(new WebSocketTransport(new StandardWebSocketClient()))));
-        stompClient.setMessageConverter(new MappingJackson2MessageConverter());
+        stompClient.setMessageConverter(new StringMessageConverter());
         StompHeaders stompHeaders = new StompHeaders();
-        stompHeaders.add("user", user);
+        stompHeaders.add("token", token);
+        stompHeaders.add("type", type);
         ListenableFuture<StompSession> future = stompClient.connect(baseUrl, (WebSocketHttpHeaders) null, stompHeaders, new StompSessionHandlerAdapter() {});
         StompSession stompSession = future.get(1, TimeUnit.SECONDS);
+        stompSession.subscribe("/user/queue/onsdp", new StompFrameHandler() {
+            @Override
+            public Type getPayloadType(StompHeaders headers) {
+                return String.class;
+            }
 
-        stompSession.subscribe(wsOnErrors, new WSErrorHandler(stompSession));
-        stompSession.subscribe(wsOffer, new WSOfferHandler(stompSession));
-        stompSession.send(wsRegister + "/" + token, null);
-
-        //2.pair
-        baseUrl = "http://localhost:" + port + "/pair";
-        URI uri = new URI(baseUrl);
-        PairVO pairVO = new PairVO();
-        pairVO.setName(user);
-        pairVO.setToken(token);
-
-        HttpHeaders headers = new HttpHeaders();
-        HttpEntity<PairVO> pairRequest = new HttpEntity<>(pairVO, headers);
-        ResponseEntity<String> result = restTemplate.postForEntity(uri, pairRequest, String.class);
-        String cookie = result.getHeaders().getFirst(HttpHeaders.SET_COOKIE);
-        if (result.getStatusCode() == HttpStatus.OK) {
-            baseUrl = "http://localhost:" + port + "/offer";
-            uri = new URI(baseUrl);
-            SdpVO sdpVO = new SdpVO();
-            sdpVO.setSdp(offer);
-            headers = new HttpHeaders();
-            headers.add("Cookie", cookie);
-            HttpEntity<SdpVO> offerRequest = new HttpEntity<>(sdpVO, headers);
-            result = restTemplate.postForEntity(uri, offerRequest, String.class);
-        }
-        logger.info(result.toString());
-    }
-
-    private class WSErrorHandler implements StompFrameHandler {
-
-        StompSession stompSession;
-
-        public WSErrorHandler(StompSession stompSession) {
-            this.stompSession = stompSession;
-        }
-
-        @Override
-        public Type getPayloadType(StompHeaders headers) {
-            return String.class;
-        }
-
-        @Override
-        public void handleFrame(StompHeaders headers, Object payload) {
-            logger.info("{} errors --> {}", stompSession.getSessionId(), payload);
-        }
-    }
-
-    private class WSOfferHandler implements StompFrameHandler {
-
-        StompSession stompSession;
-
-        public WSOfferHandler(StompSession stompSession) {
-            this.stompSession = stompSession;
-        }
-
-        @Override
-        public Type getPayloadType(StompHeaders headers) {
-            return SdpVO.class;
-        }
-
-        @Override
-        public void handleFrame(StompHeaders headers, Object payload) {
-            logger.info("{} 收到offer --> {}", stompSession.getSessionId(), payload.toString());
-            StompSession.Receiptable receipt = stompSession.send("/app/answer", answer);
-            receipt.addReceiptLostTask(() -> logger.info("{} lost answer", stompSession.getSessionId()));
-        }
+            @Override
+            public void handleFrame(StompHeaders headers, Object payload) {
+                LOGGER.info(type + " receive " + payload);
+                if(type.equals("answer")){
+                    stompSession.send("/app/sdp", "answer's sdp");
+                }
+            }
+        });
+        return stompSession;
     }
 }
