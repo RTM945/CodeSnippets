d7728d96689eb7a173c1c1a40f22e11fb466ee0e
RTM <assiadamo@gmail.com>
2020-06-05 23:56:53 +0800

    java bintree

---
+++ java/common/src/algorithms/ArrayBinTree.java
@@ -0,0 +1,14 @@
+package algorithms;
+
+public class ArrayBinTree<T> {
+    private int capacity;
+
+    private final Object[] datas;
+
+    public ArrayBinTree(int capacity) {
+        this.capacity = capacity;
+        this.datas = new Object[capacity];
+    }
+
+    
+}
