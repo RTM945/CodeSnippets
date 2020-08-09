0c6fd13297a4e38a55b4db31edc6f127571b12d9
RTM <assiadamo@gmail.com>
2020-08-09 22:45:14 +0800

    leetcode 93

---
+++ java/test/src/main/java/algorithms/leetcode/_093_restore_ip_addresses/_093.java
@@ -0,0 +1,47 @@
+package algorithms.leetcode._093_restore_ip_addresses;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// 给定一个只包含数字的字符串，复原它并返回所有可能的 IP 地址格式。
+// 有效的 IP 地址正好由四个整数（每个整数位于 0 到 255 之间组成），整数之间用 '.' 分隔。
+// 示例:
+// 输入: "25525511135"
+// 输出: ["255.255.11.135", "255.255.111.35"]
+// 来源：力扣（LeetCode）
+// 链接：https://leetcode-cn.com/problems/restore-ip-addresses
+// 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+public class _093 {
+    class Solution {
+        // 先暴力
+        public List<String> restoreIpAddresses(String s) {
+            List<String> list = new ArrayList<>();
+            if (s.length() > 12) {
+                return list;
+            }
+            StringBuilder ip = new StringBuilder();
+            for (int a = 1; a < 4; a++) {
+                for (int b = 1; b < 4; a++) {
+                    for (int c = 1; c < 4; a++) {
+                        for (int d = 1; d < 4; a++) {
+                            if (a + b + c + d == s.length()) {
+                                int n1 = Integer.parseInt(s.substring(0, a));
+                                int n2 = Integer.parseInt(s.substring(a, a + b));
+                                int n3 = Integer.parseInt(s.substring(a + b, a + b + c));
+                                int n4 = Integer.parseInt(s.substring(a + b + c));
+                                if (n1 <= 255 && n2 <= 255 && n3 <= 255 && n4 <= 255) {
+                                    ip.append(n1).append('.').append(n2).append('.').append(n3).append('.').append(n4);
+                                    if (ip.length() == s.length() + 3) {
+                                        list.add(ip.toString());
+                                    }
+                                    ip.delete(0, ip.length());
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            return list;
+        }
+    }
+}
