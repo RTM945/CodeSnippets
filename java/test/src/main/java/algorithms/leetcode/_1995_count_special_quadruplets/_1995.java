5f44700edde1482eb4aeb13b91024cf4916f3176
RTM945 <assiadamo@gmail.com>
2021-12-29 11:18:17 +0800

    leetcode 1995

--- java/test/src/main/java/algorithms/leetcode/_1995_count_special_quadruplets/_1995.java
+++ java/test/src/main/java/algorithms/leetcode/_1995_count_special_quadruplets/_1995.java
@@ -1,5 +1,8 @@
 package algorithms.leetcode._1995_count_special_quadruplets;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class _1995 {
     // 给你一个 下标从 0 开始 的整数数组 nums ，
     // 返回满足下述条件的 不同 四元组 (a, b, c, d) 的 数目 ：
@@ -27,6 +30,45 @@ public class _1995 {
                 }
             }
             return result;
+        }
+    }
+
+    // a b c 确定时 d 也就确定了 可以少一个循环
+    class Solution1 {
+        int countQuadruplets(int[] nums) {
+            int n = nums.length;
+            int ans = 0;
+            Map<Integer, Integer> cnt = new HashMap<>();
+            // 假设最后一位是 d 倒数第二位是 c 从倒数第二位向前遍历到假定的 b 第二位
+            for (int c = n - 2; c >= 2; --c) {
+                // 这段都有可能为 d 记录下来
+                cnt.put(nums[c + 1], cnt.getOrDefault(nums[c + 1], 0) + 1);
+                for (int a = 0; a < c; ++a) {
+                    for (int b = a + 1; b < c; ++b) {
+                        ans += cnt.getOrDefault(nums[a] + nums[b] + nums[c], 0);
+                    }
+                }
+            }
+            return ans;
+        }
+    }
+
+    // a b 确定时 d - c 也就确定了 可以再少一个循环
+    class Solution2 {
+        int countQuadruplets(int[] nums) {
+            int n = nums.length;
+            int ans = 0;
+            Map<Integer, Integer> cnt = new HashMap<>();
+            // 假设最后一位是 d 倒数第三位是 b 从倒数第二位向前遍历到假定的 a 第一位
+            for (int b = n - 3; b >= 1; --b) {
+                for (int d = b + 2; d < n; ++d) {
+                    cnt.put(nums[d] - nums[b + 1], cnt.getOrDefault(nums[d] - nums[b + 1], 0) + 1);
+                }
+                for (int a = 0; a < b; ++a) {
+                    ans += cnt.getOrDefault(nums[a] + nums[b], 0);
+                }
+            }
+            return ans;
         }
     }
 }
