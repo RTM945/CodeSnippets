dc898a099e7f433ea09c4a0e9fb26824b14cdf99
rtmsoft <ghcbeyond@hotmail.com>
2022-03-27 23:24:22 +0800

    leetcode 2028

---
+++ java/test/src/main/java/algorithms/leetcode/_2028_find_missing_observations/_2028.java
@@ -0,0 +1,44 @@
+package algorithms.leetcode._2028_find_missing_observations;
+
+public class _2028 {
+    // 现有一份 n + m 次投掷单个 六面 骰子的观测数据，骰子的每个面从 1 到 6 编号。
+    // 观测数据中缺失了 n 份，你手上只拿到剩余 m 次投掷的数据。
+    // 幸好你有之前计算过的这 n + m 次投掷数据的 平均值 。
+
+    // 给你一个长度为 m 的整数数组 rolls ，其中 rolls[i] 是第 i 次观测的值。
+    // 同时给你两个整数 mean 和 n 。
+
+    // 返回一个长度为 n 的数组，包含所有缺失的观测数据，
+    // 且满足这 n + m 次投掷的 平均值 是 mean 。
+    // 如果存在多组符合要求的答案，只需要返回其中任意一组即可。如果不存在答案，返回一个空数组。
+
+    // k 个数字的 平均值 为这些数字求和后再除以 k 。
+
+    // 注意 mean 是一个整数，所以 n + m 次投掷的总和需要被 n + m 整除。
+
+    // 来源：力扣（LeetCode）
+    // 链接：https://leetcode-cn.com/problems/find-missing-observations
+    // 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+    class Solution {
+        public int[] missingRolls(int[] rolls, int mean, int n) {
+            int sum = 0;
+            int m = rolls.length;
+            for (int i = 0; i < m; i++) {
+                sum += rolls[i];
+            }
+            int total = (n + m) * mean;
+            int remain = total - sum;
+            // n 个 1 到 6 的数字, 和是 remain
+            if (remain < n || remain > 6 * n) {
+                return new int[0];
+            }
+            int[] res = new int[n];
+            int a = remain / n;
+            int b = remain % n;
+            for (int i = 0; i < n; i++) {
+                res[i] = a + (i < b ? 1 : 0);
+            }
+            return res;
+        }
+    }
+}
