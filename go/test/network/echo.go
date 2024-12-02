c5d1beab6f4613b1b9d75eeb14776e3d95ee1e86
任天梦 <assiadamo@gmail.com>
2024-12-02 17:16:48 +0800

    Update echo.go

--- go/test/network/echo.go
+++ go/test/network/echo.go
@@ -1,17 +1,24 @@
 package network
 
 import (
+	"bytes"
 	"github.com/golang/protobuf/proto"
 	"gotest/network/protobuf"
 )
 
 type Echo struct {
+	MsgBase
 	msg string
 }
 
-func (e *Echo) Decode(buffer []byte) error {
+func (e *Echo) Init(header *MsgHeader, session *Session) {
+	e.MsgBase.header = header
+	e.MsgBase.session = session
+}
+
+func (e *Echo) Decode(buffer *bytes.Buffer) error {
 	tmp := &protobuf.Echo{}
-	err := proto.Unmarshal(buffer, tmp)
+	err := proto.Unmarshal(buffer.Bytes(), tmp)
 	if err != nil {
 		return err
 	}
@@ -19,6 +26,7 @@ func (e *Echo) Decode(buffer []byte) error {
 	return nil
 }
 
-func (e *Echo) GetProtoID() uint32 {
-	return 1
+func (e *Echo) Encode() ([]byte, error) {
+	buffer, err := proto.Marshal(&protobuf.Echo{Msg: e.msg})
+	return buffer, err
 }
