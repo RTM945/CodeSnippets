221ec7f253383839f49cdb8cdb7fff79ccb0e3b9
任天梦 <assiadamo@gmail.com>
2024-12-05 17:54:50 +0800

    Create session.go

---
+++ go/test/network/io/session.go
@@ -0,0 +1,40 @@
+package io
+
+import (
+	"log"
+	"net"
+	"sync"
+)
+
+var sessions sync.Map
+
+type Session struct {
+	Conn net.Conn
+}
+
+func NewSession(conn net.Conn) *Session {
+	session := &Session{
+		Conn: conn,
+	}
+	sessions.Store(session, 0)
+	return session
+}
+
+func RemoveSession(session *Session) {
+	sessions.Delete(session)
+}
+
+func (s *Session) Send(msg Msg) {
+	buffer := GetBuffer()
+	err := EncodeMsg(s, buffer, msg)
+	if err != nil {
+		log.Println("encode msg error:", err)
+		PutBuffer(buffer)
+	}
+	_, err = s.Conn.Write(buffer.Bytes())
+	if err != nil {
+		log.Println("send msg error:", err)
+		PutBuffer(buffer)
+	}
+	PutBuffer(buffer)
+}
