4a7773e8925455c90b877330cf687d36422fd73f
任天梦 <assiadamo@gmail.com>
2024-12-06 17:20:39 +0800

    Update session.go

--- go/test/network/session.go
+++ go/test/network/session.go
@@ -1,6 +1,7 @@
-package network
+package io
 
 import (
+	"log"
 	"net"
 	"sync"
 )
@@ -21,4 +22,21 @@ func NewSession(conn net.Conn) *Session {
 
 func RemoveSession(session *Session) {
 	sessions.Delete(session)
+}
+
+func (s *Session) Send(msg Msg) {
+	buffer := GetBuffer()
+	err := EncodeMsg(s, buffer, msg)
+	if err != nil {
+		log.Println("encode msg error:", err)
+		PutBuffer(buffer)
+		return
+	}
+	_, err = s.Conn.Write(buffer.Bytes())
+	if err != nil {
+		log.Println("send msg error:", err)
+		PutBuffer(buffer)
+		return
+	}
+	PutBuffer(buffer)
 }
