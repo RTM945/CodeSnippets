5dced8519afc5f73184aa373198a8840540673fc
任天梦 <assiadamo@gmail.com>
2024-12-09 19:30:24 +0800

    Create secho.go

---
+++ go/reares/proto/secho.go
@@ -0,0 +1,58 @@
+package proto
+
+import (
+	"bytes"
+	"google.golang.org/protobuf/proto"
+	"reares/internal/io"
+	"reares/protobuf"
+)
+
+type SEcho struct {
+	*io.MsgBase
+	MsgProcessor
+	Msg string
+}
+
+func NewSEchoWithProcessor(msgProcessor MsgProcessor) *SEcho {
+	header := &io.MsgHeader{}
+	header.TypeId = 2
+	return &SEcho{
+		MsgBase:      io.NewMsgBase(header),
+		MsgProcessor: msgProcessor,
+	}
+}
+
+func NewSEcho() *SEcho {
+	header := &io.MsgHeader{}
+	header.TypeId = 2
+	return &SEcho{
+		MsgBase: io.NewMsgBase(header),
+	}
+}
+
+func (echo *SEcho) Decode(src *bytes.Buffer) error {
+	tmp := &protobuf.Echo{}
+	err := proto.Unmarshal(src.Bytes(), tmp)
+	if err != nil {
+		return err
+	}
+	echo.Msg = tmp.Msg
+	return nil
+}
+
+func (echo *SEcho) Encode(dst *bytes.Buffer) error {
+	data, err := proto.Marshal(&protobuf.Echo{Msg: echo.Msg})
+	if err != nil {
+		return err
+	}
+	_, err = dst.Write(data)
+	return err
+}
+
+func (echo *SEcho) Dispatch() {
+	echo.Process()
+}
+
+func (echo *SEcho) Process() error {
+	return echo.ProcessSEcho(echo)
+}
