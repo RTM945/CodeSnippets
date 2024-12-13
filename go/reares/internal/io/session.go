59afe3da1b884c1ed7eb51c39cde3ca32af57143
任天梦 <assiadamo@gmail.com>
2024-12-13 17:31:17 +0800

    Update session.go

--- go/reares/internal/io/session.go
+++ go/reares/internal/io/session.go
@@ -2,36 +2,30 @@ package io
 
 import (
 	"net"
-	"sync"
 	"sync/atomic"
 )
 
-var sessions sync.Map
 var genSessionId int32
 
-type Session struct {
+type Session interface {
+	Send(msg Msg) error
+	GetSid() int32
+	OnClose()
+}
+
+type DefaultSession struct {
 	conn net.Conn
 	sid  int32
 }
 
-func NewSession(conn net.Conn) *Session {
-	session := &Session{
+func NewDefaultSession(conn net.Conn) *DefaultSession {
+	return &DefaultSession{
 		conn: conn,
 		sid:  atomic.AddInt32(&genSessionId, 1),
 	}
-	sessions.Store(session.sid, session)
-	return session
 }
 
-func RemoveSession(session *Session) {
-	sessions.Delete(session.sid)
-}
-
-func (session *Session) SetConn(conn net.Conn) {
-	session.conn = conn
-}
-
-func (session *Session) Send(msg Msg) error {
+func (session *DefaultSession) Send(msg Msg) error {
 	buffer := GetBuffer()
 	err := EncodeMsg(buffer, msg)
 	if err != nil {
@@ -47,32 +41,42 @@ func (session *Session) Send(msg Msg) error {
 	return nil
 }
 
-// type Session interface {
-// 	Send(msg Msg) error
-// 	SetConn(conn net.Conn)
-// }
+func (session *DefaultSession) GetSid() int32 {
+	return session.sid
+}
 
-// type SessionBase struct {
-// 	conn net.Conn
-// 	sid  int32
-// }
+func (session *DefaultSession) OnClose() {}
 
-// func (session *SessionBase) Send(msg Msg) error {
-// 	buffer := GetBuffer()
-// 	err := EncodeMsg(session, buffer, msg)
-// 	if err != nil {
-// 		PutBuffer(buffer)
-// 		return err
-// 	}
-// 	_, err = session.conn.Write(buffer.Bytes())
-// 	if err != nil {
-// 		PutBuffer(buffer)
-// 		return err
-// 	}
-// 	PutBuffer(buffer)
-// 	return nil
-// }
+type StateSession struct {
+	*DefaultSession
+	state int32
+}
 
-// func (session *SessionBase) SetConn(conn net.Conn) {
-// 	session.conn = conn
-// }
+func NewStateSession(conn net.Conn) *StateSession {
+	return &StateSession{
+		DefaultSession: NewDefaultSession(conn),
+	}
+}
+
+func (session *StateSession) AddState(state int32) {
+	atomic.AddInt32(&session.state, state)
+}
+
+func (session *StateSession) GetState() int32 {
+	return atomic.LoadInt32(&session.state)
+}
+
+func (session *StateSession) CheckState(state int32) bool {
+	currentState := atomic.LoadInt32(&session.state)
+	return state == (currentState & state)
+}
+
+func (session *StateSession) RemoveState(state int32) {
+	for {
+		oldState := atomic.LoadInt32(&session.state)
+		newState := oldState &^ state
+		if atomic.CompareAndSwapInt32(&session.state, oldState, newState) {
+			break
+		}
+	}
+}
