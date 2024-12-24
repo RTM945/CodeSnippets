41130ce4e3cf324f49da3e67ae39a1e45fc627b6
任天梦 <assiadamo@gmail.com>
2024-12-24 15:26:24 +0800

    Update shard.go

--- go/reares/cmd/go-netty/shard.go
+++ go/reares/cmd/go-netty/shard.go
@@ -1,24 +1,48 @@
 package shard
 
 import (
+	"encoding/binary"
 	"fmt"
 	"github.com/go-netty/go-netty"
+	"github.com/go-netty/go-netty/codec"
+	"github.com/go-netty/go-netty/codec/frame"
 	"github.com/go-netty/go-netty/utils"
 	"github.com/go-netty/go-netty/utils/pool/pbuffer"
+	"math"
 	"reares/internal/io"
 )
+
+var LengthFieldBasedFrameDecoder = lengthFieldBasedFrameDecoder{
+	frame.LengthFieldCodec(binary.BigEndian, math.MaxInt, 0, 4, 0, 4),
+}
+
+type lengthFieldBasedFrameDecoder struct {
+	codec codec.Codec
+}
+
+func (fc lengthFieldBasedFrameDecoder) HandleRead(ctx netty.InboundContext, message netty.Message) {
+	fc.codec.HandleRead(ctx, message)
+}
+
+func (fc lengthFieldBasedFrameDecoder) HandleWrite(ctx netty.OutboundContext, message netty.Message) {
+	// 覆盖LengthFieldCodec的实现
+	// 这里什么都不做直接交给下一个处理器
+	ctx.HandleWrite(message)
+}
 
 type MsgEncoder struct{}
 
 func (MsgEncoder) HandleWrite(ctx netty.OutboundContext, message netty.Message) {
 	msg := message.(io.Msg)
+	fmt.Println("encode:", msg.GetHeader().TypeId)
 	buffer := pbuffer.Get(4096)
 	buffer.Reset()
-	//err := binary.Write(buffer, binary.BigEndian, uint32(0))
-	//if err != nil {
-	//	panic(err)
-	//}
 	var err error
+	err = binary.Write(buffer, binary.BigEndian, uint32(0))
+	if err != nil {
+		panic(err)
+	}
+
 	header := msg.GetHeader()
 	err = header.Encode(buffer)
 	if err != nil {
@@ -28,12 +52,9 @@ func (MsgEncoder) HandleWrite(ctx netty.OutboundContext, message netty.Message) 
 	if err != nil {
 		panic(err)
 	}
-	//totalLen := buffer.Len() - 4
-	//data := buffer.Bytes()
-	//binary.BigEndian.PutUint32(data[:4], uint32(totalLen))
-	if msg.GetHeader().TypeId == 2 {
-		fmt.Println()
-	}
+	totalLen := buffer.Len() - 4
+	data := buffer.Bytes()
+	binary.BigEndian.PutUint32(data[:4], uint32(totalLen))
 	ctx.HandleWrite(buffer)
 	pbuffer.Put(buffer)
 }
@@ -55,6 +76,7 @@ func (MsgDecoder) HandleRead(ctx netty.InboundContext, message netty.Message) {
 	if err != nil {
 		panic(err)
 	}
+	fmt.Println("decode:", msg.GetHeader().TypeId)
 	ctx.HandleRead(msg)
 }
 
