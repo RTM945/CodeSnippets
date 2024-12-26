a817a82e94d34584d6aa083675f607cfc527ce76
任天梦 <assiadamo@gmail.com>
2024-12-26 19:53:56 +0800

    Update main.go

--- go/reares/cmd/go-netty/client/main.go
+++ go/reares/cmd/go-netty/client/main.go
@@ -3,28 +3,32 @@ package main
 import (
 	"github.com/go-netty/go-netty"
 	shard "reares/cmd/go-netty"
-	"reares/logic"
-	"reares/proto/echo"
+	"reares/cmd/go-netty/logic"
 	"time"
 )
 
+const Service = "Client"
+
 func main() {
+	Init()
 	logic.Init()
 	var childInitializer = func(channel netty.Channel) {
 		channel.Pipeline().
 			AddLast(shard.MsgEncoder{}).
 			AddLast(shard.LengthFieldBasedFrameDecoder).
 			AddLast(shard.MsgDecoder{}).
-			AddLast(shard.LogicHandler{})
+			AddLast(shard.LogicHandler{
+				NodeFactory: NodeFactory{},
+			})
 	}
-	client, err := netty.NewBootstrap(
+	_, err := netty.NewBootstrap(
 		netty.WithClientInitializer(childInitializer),
 	).Connect("127.0.0.1:9527")
 	if err != nil {
 		return
 	}
-	echo := echo.NewCEcho()
-	echo.Msg = "test"
-	client.Write(echo)
-	time.Sleep(time.Second * 10)
+	//echo := echo.NewCEcho()
+	//echo.Msg = "test"
+	//client.Write(echo)
+	time.Sleep(time.Second * 100)
 }
