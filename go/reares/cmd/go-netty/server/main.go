3bf3408c7991a8df6df7962ec8220632dc63ed3d
任天梦 <assiadamo@gmail.com>
2024-12-26 19:54:18 +0800

    Update main.go

--- go/reares/cmd/go-netty/server/main.go
+++ go/reares/cmd/go-netty/server/main.go
@@ -3,19 +3,22 @@ package main
 import (
 	"github.com/go-netty/go-netty"
 	shard "reares/cmd/go-netty"
-	"reares/logic"
+	"reares/cmd/go-netty/logic"
 )
 
-type LoggerHandler struct{}
+const Service = "Linker"
 
 func main() {
+	Init()
 	logic.Init()
 	var childInitializer = func(channel netty.Channel) {
 		channel.Pipeline().
 			AddLast(shard.MsgEncoder{}).
 			AddLast(shard.LengthFieldBasedFrameDecoder).
 			AddLast(shard.MsgDecoder{}).
-			AddLast(shard.LogicHandler{})
+			AddLast(shard.LogicHandler{
+				NodeFactory: NodeFactory{},
+			})
 	}
 	err := netty.NewBootstrap(netty.WithChildInitializer(childInitializer)).
 		Listen(":9527").Sync()
