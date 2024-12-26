c4fe493d8c28b3b4f6f541b8045b066051ef6b7c
任天梦 <assiadamo@gmail.com>
2024-12-26 19:52:40 +0800

    Update shard.go

--- go/reares/cmd/go-netty/shard.go
+++ go/reares/cmd/go-netty/shard.go
@@ -1,42 +1,46 @@
 package shard
 
 import (
+	"bytes"
 	"encoding/binary"
-	"fmt"
 	"github.com/go-netty/go-netty"
 	"github.com/go-netty/go-netty/codec"
 	"github.com/go-netty/go-netty/codec/frame"
 	"github.com/go-netty/go-netty/utils"
-	"github.com/go-netty/go-netty/utils/pool/pbuffer"
+	"github.com/tidwall/transform"
+	"log"
 	"math"
-	"reares/internal/io"
+	"reares/pkg/rc4"
 )
 
+type Session interface {
+	Send(msg Msg) error
+	GetSid() int32
+	OnClose()
+}
+
 var LengthFieldBasedFrameDecoder = lengthFieldBasedFrameDecoder{
-	frame.LengthFieldCodec(binary.BigEndian, math.MaxInt, 0, 4, 0, 4),
+	codec: frame.LengthFieldCodec(binary.BigEndian, math.MaxInt, 0, 4, 0, 4),
 }
 
 type lengthFieldBasedFrameDecoder struct {
 	codec codec.Codec
 }
 
-func (fc lengthFieldBasedFrameDecoder) HandleRead(ctx netty.InboundContext, message netty.Message) {
-	fc.codec.HandleRead(ctx, message)
+func (l lengthFieldBasedFrameDecoder) HandleRead(ctx netty.InboundContext, message netty.Message) {
+	l.codec.HandleRead(ctx, message)
 }
 
-func (fc lengthFieldBasedFrameDecoder) HandleWrite(ctx netty.OutboundContext, message netty.Message) {
-	// 覆盖LengthFieldCodec的实现
-	// 这里什么都不做直接交给下一个处理器
+func (lengthFieldBasedFrameDecoder) HandleWrite(ctx netty.OutboundContext, message netty.Message) {
 	ctx.HandleWrite(message)
 }
 
-type MsgEncoder struct{}
+type MsgEncoder struct {
+}
 
-func (MsgEncoder) HandleWrite(ctx netty.OutboundContext, message netty.Message) {
-	msg := message.(io.Msg)
-	fmt.Println("encode:", msg.GetHeader().TypeId)
-	buffer := pbuffer.Get(4096)
-	buffer.Reset()
+func (e MsgEncoder) HandleWrite(ctx netty.OutboundContext, message netty.Message) {
+	msg := message.(Msg)
+	buffer := bytes.NewBuffer(nil)
 	var err error
 	err = binary.Write(buffer, binary.BigEndian, uint32(0))
 	if err != nil {
@@ -55,64 +59,120 @@ func (MsgEncoder) HandleWrite(ctx netty.OutboundContext, message netty.Message) 
 	totalLen := buffer.Len() - 4
 	data := buffer.Bytes()
 	binary.BigEndian.PutUint32(data[:4], uint32(totalLen))
+	log.Println("send ", msg.GetHeader().TypeId)
 	ctx.HandleWrite(buffer)
-	pbuffer.Put(buffer)
 }
 
-type MsgDecoder struct{}
+type MsgDecoder struct {
+}
 
-func (MsgDecoder) HandleRead(ctx netty.InboundContext, message netty.Message) {
-	session := ctx.Channel().Attachment().(io.Session)
-	data := utils.MustToBytes(message)
-	buffer := pbuffer.Get(len(data))
-	buffer.Reset()
-	buffer.Write(data)
-	header := &io.MsgHeader{}
+func (d MsgDecoder) HandleRead(ctx netty.InboundContext, message netty.Message) {
+	session := ctx.Channel().Attachment().(Session)
+	buffer := bytes.NewBuffer(utils.MustToBytes(message))
+	header := &MsgHeader{}
 	err := header.Decode(buffer)
 	if err != nil {
 		panic(err)
 	}
-	msg, err := io.CreateMsg(header, session, buffer)
+	msg, err := CreateMsg(header, session, buffer)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("decode:", msg.GetHeader().TypeId)
+	log.Println("recv:", msg.GetHeader().TypeId)
 	ctx.HandleRead(msg)
 }
 
-type LogicHandler struct{}
+type SessionFactory interface {
+	CreateSession(channel netty.Channel) Session
+}
 
-func (LogicHandler) HandleActive(ctx netty.ActiveContext) {
-	ctx.Channel().SetAttachment(session{channel: ctx.Channel()})
+type SessionManager interface {
+	OnAddSession(session Session)
+	OnRemoveSession(session Session)
+}
+
+type NodeFactory interface {
+	SessionFactory
+	SessionManager
+}
+
+type LogicHandler struct {
+	NodeFactory NodeFactory
+}
+
+func (l LogicHandler) HandleActive(ctx netty.ActiveContext) {
+	session := l.NodeFactory.CreateSession(ctx.Channel())
+	ctx.Channel().SetAttachment(session)
+	l.NodeFactory.OnAddSession(session)
 	ctx.HandleActive()
 }
 
 func (LogicHandler) HandleRead(ctx netty.InboundContext, message netty.Message) {
-	msg := message.(io.Msg)
+	msg := message.(Msg)
 	msg.Dispatch()
 	ctx.HandleRead(message)
 }
 
-func (LogicHandler) HandleInactive(ctx netty.InactiveContext, ex netty.Exception) {
-	session := ctx.Channel().Attachment().(io.Session)
+func (l LogicHandler) HandleInactive(ctx netty.InactiveContext, ex netty.Exception) {
+	session := ctx.Channel().Attachment().(Session)
+	l.NodeFactory.OnRemoveSession(session)
 	session.OnClose()
 	ctx.HandleInactive(ex)
 }
 
 func (LogicHandler) HandleException(ctx netty.ExceptionContext, ex netty.Exception) {
-	fmt.Println("HandleException:", ex)
+	log.Println("HandleException:", ex)
 }
 
-type session struct {
-	channel netty.Channel
+type SecurityEncoder struct {
+	RC4 *rc4.RC4
 }
 
-func (s session) Send(msg io.Msg) error {
-	return s.channel.Write(msg)
+func (SecurityEncoder) CodecName() string {
+	return "SecurityEncoder"
 }
 
-func (s session) GetSid() int32 {
-	return 0
+func (se SecurityEncoder) HandleRead(ctx netty.InboundContext, message netty.Message) {
+	ctx.HandleRead(message)
 }
 
-func (s session) OnClose() {}
+func (se SecurityEncoder) HandleWrite(ctx netty.OutboundContext, message netty.Message) {
+	reader := utils.MustToReader(message)
+	buf := make([]byte, 1024)
+	trans := transform.NewTransformer(func() ([]byte, error) {
+		n, err := reader.Read(buf)
+		if err != nil {
+			return nil, err
+		}
+		se.RC4.DoUpdate(buf)
+		return buf[:n], nil
+	})
+
+	ctx.HandleWrite(trans)
+}
+
+type SecurityDecoder struct {
+	RC4 *rc4.RC4
+}
+
+func (SecurityDecoder) CodecName() string {
+	return "SecurityDecoder"
+}
+
+func (sd SecurityDecoder) HandleRead(ctx netty.InboundContext, message netty.Message) {
+	reader := utils.MustToReader(message)
+	buf := make([]byte, 1024)
+	trans := transform.NewTransformer(func() ([]byte, error) {
+		n, err := reader.Read(buf)
+		if err != nil {
+			return nil, err
+		}
+		sd.RC4.DoUpdate(buf)
+		return buf[:n], nil
+	})
+	ctx.HandleRead(trans)
+}
+
+func (sd SecurityDecoder) HandleWrite(ctx netty.OutboundContext, message netty.Message) {
+	ctx.HandleWrite(message)
+}
