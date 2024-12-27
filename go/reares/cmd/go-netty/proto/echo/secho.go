547345225ecbd991b70b814d4879ce3365658b3a
任天梦 <assiadamo@gmail.com>
2024-12-27 14:35:53 +0800

    Create secho.go

---
+++ go/reares/cmd/go-netty/proto/echo/secho.go
@@ -0,0 +1,58 @@
+package echo
+
+import (
+	"bytes"
+	"google.golang.org/protobuf/proto"
+	shard "reares/cmd/go-netty"
+	"reares/protobuf"
+)
+
+type SEcho struct {
+	*shard.MsgBase
+	Processor
+	Msg string
+}
+
+func InitSEcho(msgProcessor Processor) *SEcho {
+	header := &shard.MsgHeader{}
+	header.TypeId = 101
+	return &SEcho{
+		MsgBase:   shard.NewMsgBase(header),
+		Processor: msgProcessor,
+	}
+}
+
+func NewSEcho() *SEcho {
+	header := &shard.MsgHeader{}
+	header.TypeId = 101
+	return &SEcho{
+		MsgBase: shard.NewMsgBase(header),
+	}
+}
+
+func (echo *SEcho) Decode(src *bytes.Buffer) error {
+	tmp := &protobuf.Echo{}
+	err := proto.Unmarshal(src.Bytes(), tmp)
+	if err != nil {
+		return err
+	}
+	echo.Msg = tmp.Msg
+	return nil
+}
+
+func (echo *SEcho) Encode(dst *bytes.Buffer) error {
+	data, err := proto.Marshal(&protobuf.Echo{Msg: echo.Msg})
+	if err != nil {
+		return err
+	}
+	_, err = dst.Write(data)
+	return err
+}
+
+func (echo *SEcho) Dispatch() {
+	echo.Process()
+}
+
+func (echo *SEcho) Process() error {
+	return echo.ProcessSEcho(echo)
+}
