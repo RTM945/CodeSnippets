35d20d609558efafc68b85398be807e5988b8a63
RTM945 <assiadamo@gmail.com>
2025-02-10 18:42:34 +0800

    graph

---
+++ unity/test/Assets/Scripts/Graph.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class Graph : MonoBehaviour {
+
+    [SerializeField]
+    Transform pointPrefab;
+
+    [SerializeField, Range(10, 100)]
+    int resolution = 10;
+
+    Transform[] points;
+
+    void Awake () {
+        float step = 2f / resolution;
+        var position = Vector3.zero;
+        var scale = Vector3.one * step;
+        points = new Transform[resolution];
+        for (int i = 0; i < points.Length; i++) {
+            Transform point = points[i] = Instantiate(pointPrefab);
+            position.x = (i + 0.5f) * step - 1f;
+            point.localPosition = position;
+            point.localScale = scale;
+            point.SetParent(transform, false);
+        }
+    }
+
+    void Update () {
+        float time = Time.time;
+        for (int i = 0; i < points.Length; i++) {
+            Transform point = points[i];
+            Vector3 position = point.localPosition;
+            position.y = Mathf.Sin(Mathf.PI * (position.x + time));
+            point.localPosition = position;
+        }
+    }
+}
