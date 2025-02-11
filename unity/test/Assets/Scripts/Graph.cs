b17008c4c03413e8a00b56c69dff0566f9ce824c
RTM945 <assiadamo@gmail.com>
2025-02-11 17:23:31 +0800

    delegate

--- unity/test/Assets/Scripts/Graph.cs
+++ unity/test/Assets/Scripts/Graph.cs
@@ -7,30 +7,44 @@ public class Graph : MonoBehaviour {
 
     [SerializeField, Range(10, 100)]
     int resolution = 10;
+    
+    [SerializeField]
+    FunctionLibrary.FunctionName function;
 
     Transform[] points;
 
     void Awake () {
         float step = 2f / resolution;
-        var position = Vector3.zero;
         var scale = Vector3.one * step;
-        points = new Transform[resolution];
+        //var position = Vector3.zero;
+        points = new Transform[resolution * resolution];
         for (int i = 0; i < points.Length; i++) {
+            //if (x == resolution) {
+            //	x = 0;
+            //	z += 1;
+            //}
             Transform point = points[i] = Instantiate(pointPrefab);
-            position.x = (i + 0.5f) * step - 1f;
-            point.localPosition = position;
+            //position.x = (x + 0.5f) * step - 1f;
+            //position.z = (z + 0.5f) * step - 1f;
+            //point.localPosition = position;
             point.localScale = scale;
             point.SetParent(transform, false);
         }
     }
 
     void Update () {
+        FunctionLibrary.Function f = FunctionLibrary.GetFunction(function);
         float time = Time.time;
-        for (int i = 0; i < points.Length; i++) {
-            Transform point = points[i];
-            Vector3 position = point.localPosition;
-            position.y = Mathf.Sin(Mathf.PI * (position.x + time));
-            point.localPosition = position;
+        float step = 2f / resolution;
+        float v = 0.5f * step - 1f;
+        for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++) {
+            if (x == resolution) {
+                x = 0;
+                z += 1;
+                v = (z + 0.5f) * step - 1f;
+            }
+            float u = (x + 0.5f) * step - 1f;
+            points[i].localPosition = f(u, v, time);
         }
     }
 }
