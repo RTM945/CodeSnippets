b8d8d1e7c5b90d5bb2f855f0eb50870b90f02b5d
RTM945 <assiadamo@gmail.com>
2025-02-17 15:07:30 +0800

    compute shader

--- unity/test/Assets/Scripts/FunctionLibrary.cs
+++ unity/test/Assets/Scripts/FunctionLibrary.cs
@@ -10,19 +10,26 @@ public static class FunctionLibrary {
 
 	static Function[] functions = { Wave, MultiWave, Ripple, Sphere, Torus };
 
-	public static Function GetFunction (FunctionName name) {
-		return functions[(int)name];
-	}
+	public static int FunctionCount => functions.Length;
 
-	public static FunctionName GetNextFunctionName (FunctionName name) {
-		return (int)name < functions.Length - 1 ? name + 1 : 0;
-	}
+	public static Function GetFunction (FunctionName name) => functions[(int)name];
+
+	public static FunctionName GetNextFunctionName (FunctionName name) =>
+		(int)name < functions.Length - 1 ? name + 1 : 0;
 
 	public static FunctionName GetRandomFunctionNameOtherThan (FunctionName name) {
 		var choice = (FunctionName)Random.Range(1, functions.Length);
 		return choice == name ? 0 : choice;
 	}
-
+	
+	public static Vector3 Morph (
+		float u, float v, float t, Function from, Function to, float progress
+	) {
+		return Vector3.LerpUnclamped(
+			from(u, v, t), to(u, v, t), SmoothStep(0f, 1f, progress)
+		);
+	}
+	
 	public static Vector3 Wave (float u, float v, float t) {
 		Vector3 p;
 		p.x = u;
@@ -53,7 +60,7 @@ public static class FunctionLibrary {
 	}
 
 	public static Vector3 Sphere (float u, float v, float t) {
-		float r = 0.9f + 0.1f * Sin(PI * (6f * u + 4f * v + t));
+		float r = 0.9f + 0.1f * Sin(PI * (12f * u + 8f * v + t));
 		float s = r * Cos(0.5f * PI * v);
 		Vector3 p;
 		p.x = s * Sin(PI * u);
@@ -63,21 +70,13 @@ public static class FunctionLibrary {
 	}
 
 	public static Vector3 Torus (float u, float v, float t) {
-		float r1 = 0.7f + 0.1f * Sin(PI * (6f * u + 0.5f * t));
-		float r2 = 0.15f + 0.05f * Sin(PI * (8f * u + 4f * v + 2f * t));
+		float r1 = 0.7f + 0.1f * Sin(PI * (8f * u + 0.5f * t));
+		float r2 = 0.15f + 0.05f * Sin(PI * (16f * u + 8f * v + 3f * t));
 		float s = r1 + r2 * Cos(PI * v);
 		Vector3 p;
 		p.x = s * Sin(PI * u);
 		p.y = r2 * Sin(PI * v);
 		p.z = s * Cos(PI * u);
 		return p;
-	}
-
-	public static Vector3 Morph (
-		float u, float v, float t, Function from, Function to, float progress
-	) {
-		return Vector3.LerpUnclamped(
-			from(u, v, t), to(u, v, t), SmoothStep(0f, 1f, progress)
-		);
 	}
 }
--- unity/test/Assets/Scripts/GPUGraph.cs
+++ unity/test/Assets/Scripts/GPUGraph.cs
@@ -1,82 +1,106 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class GPUGraph : MonoBehaviour
-{
-    
-    [SerializeField, Range(10, 1000)]
-    int resolution = 10;
+public class GPUGraph : MonoBehaviour {
 
-    [SerializeField]
-    FunctionLibrary.FunctionName function;
+	const int maxResolution = 1000;
 
-    public enum TransitionMode { Cycle, Random }
+	static readonly int
+		positionsId = Shader.PropertyToID("_Positions"),
+		resolutionId = Shader.PropertyToID("_Resolution"),
+		stepId = Shader.PropertyToID("_Step"),
+		timeId = Shader.PropertyToID("_Time"),
+		transitionProgressId = Shader.PropertyToID("_TransitionProgress");
 
-    [SerializeField]
-    TransitionMode transitionMode = TransitionMode.Cycle;
+	[SerializeField]
+	ComputeShader computeShader;
 
-    [SerializeField, Min(0f)]
-    float functionDuration = 1f, transitionDuration = 1f;
-    
-    float duration;
+	[SerializeField]
+	Material material;
 
-    bool transitioning;
+	[SerializeField]
+	Mesh mesh;
 
-    FunctionLibrary.FunctionName transitionFunction;
-    
-    ComputeBuffer positionsBuffer;
-    
-    [SerializeField]
-    ComputeShader computeShader;
-    
-    [SerializeField]
-    Material material;
+	[SerializeField, Range(10, maxResolution)]
+	int resolution = 10;
 
-    [SerializeField]
-    Mesh mesh;
-    
-    static readonly int
-        positionsId = Shader.PropertyToID("_Positions"),
-        resolutionId = Shader.PropertyToID("_Resolution"),
-        stepId = Shader.PropertyToID("_Step"),
-        timeId = Shader.PropertyToID("_Time");
-    
-    void UpdateFunctionOnGPU () {
-        float step = 2f / resolution;
-        computeShader.SetInt(resolutionId, resolution);
-        computeShader.SetFloat(stepId, step);
-        computeShader.SetFloat(timeId, Time.time);
-		
-        computeShader.SetBuffer(0, positionsId, positionsBuffer);
-        int groups = Mathf.CeilToInt(resolution / 8f);
-        computeShader.Dispatch(0, groups, groups, 1);
-        material.SetBuffer(positionsId, positionsBuffer);
-        material.SetFloat(stepId, step);
-        var bounds = new Bounds(Vector3.zero, Vector3.one * (2f + 2f / resolution));
-        Graphics.DrawMeshInstancedProcedural(mesh, 0, material, bounds, positionsBuffer.count);
-    }
+	[SerializeField]
+	FunctionLibrary.FunctionName function;
 
-    void OnEnable () {
-        positionsBuffer = new ComputeBuffer(resolution * resolution, 3 * 4);
-    }
-    
-    void OnDisable () {
-        positionsBuffer.Release();
-        positionsBuffer = null;
-    }
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-    
-    void PickNextFunction () { }
+	public enum TransitionMode { Cycle, Random }
 
-    // Update is called once per frame
-    void Update()
-    {
-        UpdateFunctionOnGPU();
-    }
+	[SerializeField]
+	TransitionMode transitionMode;
+
+	[SerializeField, Min(0f)]
+	float functionDuration = 1f, transitionDuration = 1f;
+
+	float duration;
+
+	bool transitioning;
+
+	FunctionLibrary.FunctionName transitionFunction;
+
+	ComputeBuffer positionsBuffer;
+
+	void OnEnable () {
+		positionsBuffer = new ComputeBuffer(maxResolution * maxResolution, 3 * 4);
+	}
+
+	void OnDisable () {
+		positionsBuffer.Release();
+		positionsBuffer = null;
+	}
+
+	void Update () {
+		duration += Time.deltaTime;
+		if (transitioning) {
+			if (duration >= transitionDuration) {
+				duration -= transitionDuration;
+				transitioning = false;
+			}
+		}
+		else if (duration >= functionDuration) {
+			duration -= functionDuration;
+			transitioning = true;
+			transitionFunction = function;
+			PickNextFunction();
+		}
+
+		UpdateFunctionOnGPU();
+	}
+
+	void PickNextFunction () {
+		function = transitionMode == TransitionMode.Cycle ?
+			FunctionLibrary.GetNextFunctionName(function) :
+			FunctionLibrary.GetRandomFunctionNameOtherThan(function);
+	}
+
+	void UpdateFunctionOnGPU () {
+		float step = 2f / resolution;
+		computeShader.SetInt(resolutionId, resolution);
+		computeShader.SetFloat(stepId, step);
+		computeShader.SetFloat(timeId, Time.time);
+		if (transitioning) {
+			computeShader.SetFloat(
+				transitionProgressId,
+				Mathf.SmoothStep(0f, 1f, duration / transitionDuration)
+			);
+		}
+
+		var kernelIndex =
+			(int)function +
+			(int)(transitioning ? transitionFunction : function) *
+			FunctionLibrary.FunctionCount;
+		computeShader.SetBuffer(kernelIndex, positionsId, positionsBuffer);
+
+		int groups = Mathf.CeilToInt(resolution / 8f);
+		computeShader.Dispatch(kernelIndex, groups, groups, 1);
+
+		material.SetBuffer(positionsId, positionsBuffer);
+		material.SetFloat(stepId, step);
+		var bounds = new Bounds(Vector3.zero, Vector3.one * (2f + 2f / resolution));
+		Graphics.DrawMeshInstancedProcedural(
+			mesh, 0, material, bounds, resolution * resolution
+		);
+	}
 }
