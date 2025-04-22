2cf2d3143f615b2576c3926745c6bb33868d8575
RTM945 <assiadamo@gmail.com>
2025-04-22 15:03:44 +0800

    AdvancedMultiStreamProceduralMesh

---
+++ unity/test/Assets/Scripts/AdvancedMultiStreamProceduralMesh.cs
@@ -0,0 +1,80 @@
+﻿using Unity.Collections;
+using UnityEngine;
+using UnityEngine.Rendering;
+using Unity.Mathematics;
+
+using static Unity.Mathematics.math;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class AdvancedMultiStreamProceduralMesh : MonoBehaviour {
+
+	void OnEnable () {
+		int vertexAttributeCount = 4;
+		int vertexCount = 4;
+		int triangleIndexCount = 6;
+
+		Mesh.MeshDataArray meshDataArray = Mesh.AllocateWritableMeshData(1);
+		Mesh.MeshData meshData = meshDataArray[0];
+
+		var vertexAttributes = new NativeArray<VertexAttributeDescriptor>(
+			vertexAttributeCount, Allocator.Temp, NativeArrayOptions.UninitializedMemory
+		);
+		vertexAttributes[0] = new VertexAttributeDescriptor(dimension: 3);
+		vertexAttributes[1] = new VertexAttributeDescriptor(
+			VertexAttribute.Normal, dimension: 3, stream: 1
+		);
+		vertexAttributes[2] = new VertexAttributeDescriptor(
+			VertexAttribute.Tangent, VertexAttributeFormat.Float16, 4, 2
+		);
+		vertexAttributes[3] = new VertexAttributeDescriptor(
+			VertexAttribute.TexCoord0, VertexAttributeFormat.Float16, 2, 3
+		);
+		meshData.SetVertexBufferParams(vertexCount, vertexAttributes);
+		vertexAttributes.Dispose();
+
+		NativeArray<float3> positions = meshData.GetVertexData<float3>();
+		positions[0] = 0f;
+		positions[1] = right();
+		positions[2] = up();
+		positions[3] = float3(1f, 1f, 0f);
+
+		NativeArray<float3> normals = meshData.GetVertexData<float3>(1);
+		normals[0] = normals[1] = normals[2] = normals[3] = back();
+
+		half h0 = half(0f), h1 = half(1f);
+
+		NativeArray<half4> tangents = meshData.GetVertexData<half4>(2);
+		tangents[0] = tangents[1] = tangents[2] = tangents[3] =
+			half4(h1, h0, h0, half(-1f));
+		
+		NativeArray<half2> texCoords = meshData.GetVertexData<half2>(3);
+		texCoords[0] = h0;
+		texCoords[1] = half2(h1, h0);
+		texCoords[2] = half2(h0, h1);
+		texCoords[3] = h1;
+
+		meshData.SetIndexBufferParams(triangleIndexCount, IndexFormat.UInt16);
+		NativeArray<ushort> triangleIndices = meshData.GetIndexData<ushort>();
+		triangleIndices[0] = 0;
+		triangleIndices[1] = 2;
+		triangleIndices[2] = 1;
+		triangleIndices[3] = 1;
+		triangleIndices[4] = 2;
+		triangleIndices[5] = 3;
+
+		var bounds = new Bounds(new Vector3(0.5f, 0.5f), new Vector3(1f, 1f));
+
+		meshData.subMeshCount = 1;
+		meshData.SetSubMesh(0, new SubMeshDescriptor(0, triangleIndexCount) {
+			bounds = bounds,
+			vertexCount = vertexCount
+		}, MeshUpdateFlags.DontRecalculateBounds);
+
+		var mesh = new Mesh {
+			bounds = bounds,
+			name = "Procedural Mesh"
+		};
+		Mesh.ApplyAndDisposeWritableMeshData(meshDataArray, mesh);
+		GetComponent<MeshFilter>().mesh = mesh;
+	}
+}
