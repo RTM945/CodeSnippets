09cb5a4a08271b0cdd94f58551870dd9ac0073ed
RTM945 <assiadamo@gmail.com>
2025-05-07 17:07:25 +0800

    Modified Grid

--- unity/test/Assets/Mesh/ProceduralMesh.hlsl
+++ unity/test/Assets/Mesh/ProceduralMesh.hlsl
@@ -6,12 +6,11 @@ void Ripple_float (
 	float3 p = PositionIn - Origin;
 	float d = length(p);
 	float f = 2.0 * PI * Period * (d - Speed * _Time.y);
-	
+
 	PositionOut = PositionIn + float3(0.0, Amplitude * sin(f), 0.0);
 
 	float2 derivatives = (2.0 * PI * Amplitude * Period * cos(f) / max(d, 0.0001)) * p.xz;
-	
+
 	TangentOut = float3(1.0, derivatives.x, 0.0);
 	NormalOut = cross(float3(0.0, derivatives.y, 1.0), TangentOut);
-	
 }
