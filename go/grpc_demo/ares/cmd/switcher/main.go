2dd5b359c9d5c0665aa6ff5ad74a6a910bfd7652
RTM945 <assiadamo@gmail.com>
2025-07-23 19:49:57 +0800

    the easist linker and provider

--- go/grpc_demo/ares/cmd/switcher/main.go
+++ go/grpc_demo/ares/cmd/switcher/main.go
@@ -1,5 +1,262 @@
 package main
 
-func main() {
+import (
+	ares "ares/pkg/io"
+	pb "ares/proto/gen"
+	"context"
+	vtcodec "github.com/planetscale/vtprotobuf/codec/grpc"
+	"go.etcd.io/etcd/api/v3/mvccpb"
+	clientv3 "go.etcd.io/etcd/client/v3"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/peer"
+	"google.golang.org/protobuf/proto"
+	"io"
+	"log"
+	"net"
+	"sync/atomic"
+)
 
+type Linker struct {
+	pb.UnimplementedLinkerServer
+
+	sessions     map[uint32]grpc.ServerStream
+	msgProcessor map[uint32]func(msg proto.Message) error
+}
+
+func NewLinker() *Linker {
+	return &Linker{
+		sessions:     make(map[uint32]grpc.ServerStream),
+		msgProcessor: make(map[uint32]func(msg proto.Message) error),
+	}
+}
+
+func (linker *Linker) Handler(typeId uint32, handler func(msg proto.Message) error) {
+	linker.msgProcessor[typeId] = handler
+}
+
+var genSessionId atomic.Uint32
+
+func (linker *Linker) Serve(stream pb.Linker_ServeServer) error {
+	sid := genSessionId.Add(1)
+	linker.sessions[sid] = stream
+	if p, ok := peer.FromContext(stream.Context()); ok {
+		log.Println("receive client session:", p.Addr)
+	}
+	for {
+		req, err := stream.Recv()
+		if err == io.EOF {
+			return nil
+		}
+		if err != nil {
+			return err
+		}
+
+		if processor, ok := linker.msgProcessor[req.TypeId]; ok {
+			if err := processor(req); err != nil {
+				log.Printf("process %d error:%v", req.TypeId, err)
+			}
+		} else {
+			if req.PvId != 0 {
+				if session, ok := provider.sessions[req.PvId]; ok {
+					dispatch := &pb.Dispatch{
+						ClientSid: sid,
+						PvId:      req.PvId,
+						TypeId:    req.TypeId,
+						Payload:   req.Payload,
+					}
+					payload, err := dispatch.MarshalVT()
+					if err != nil {
+						log.Printf("marshal payload error:%v", err)
+						continue
+					}
+					err = session.SendMsg(&pb.Envelope{
+						TypeId:  51,
+						PvId:    req.PvId,
+						Payload: payload,
+					})
+					if err != nil {
+						log.Printf("dispatch %d to %d error:%v", req.TypeId, req.PvId, err)
+					}
+				} else {
+					log.Println("session not found:", req.PvId)
+				}
+			}
+		}
+	}
+}
+
+type Provider struct {
+	pb.UnimplementedProviderServer
+
+	sessions     map[uint32]grpc.ServerStream
+	msgProcessor map[uint32]func(msg proto.Message) error
+}
+
+func NewProvider() *Provider {
+	return &Provider{
+		sessions:     make(map[uint32]grpc.ServerStream),
+		msgProcessor: make(map[uint32]func(msg proto.Message) error),
+	}
+}
+
+func (provider *Provider) Handler(typeId uint32, handler func(msg proto.Message) error) {
+	provider.msgProcessor[typeId] = handler
+}
+
+func (provider *Provider) Serve(stream pb.Provider_ServeServer) error {
+	if p, ok := peer.FromContext(stream.Context()); ok {
+		log.Println("receive providee session", p.Addr.String())
+	}
+	for {
+		req, err := stream.Recv()
+		if err == io.EOF {
+			return nil
+		}
+		if err != nil {
+			return err
+		}
+		if processor, ok := provider.msgProcessor[req.TypeId]; ok {
+			if err := processor(req); err != nil {
+				log.Printf("process %d error:%v", req.TypeId, err)
+			}
+		} else {
+			if req.PvId != 0 {
+				if session, ok := provider.sessions[req.PvId]; ok {
+					pDispatch := &pb.PDispatch{
+						PvId:    req.PvId,
+						TypeId:  req.TypeId,
+						Payload: req.Payload,
+					}
+					payload, err := pDispatch.MarshalVT()
+					if err != nil {
+						log.Printf("marshal payload error:%v", err)
+						continue
+					}
+					err = session.SendMsg(&pb.Envelope{
+						TypeId:  77,
+						PvId:    req.PvId,
+						Payload: payload,
+					})
+					if err != nil {
+						log.Printf("pdispatch %d to %d error:%v", req.TypeId, req.PvId, err)
+					}
+				}
+			}
+		}
+	}
+}
+
+var linker = NewLinker()
+var provider = NewProvider()
+
+func main() {
+	linkerLis, err := net.Listen("tcp", ":5000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	linkerLis = ares.NewPROXYListener(linkerLis)
+
+	providerLis, err := net.Listen("tcp", ":5001")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	cli, err := clientv3.New(clientv3.Config{Endpoints: []string{"127.0.0.1:2379"}})
+	if err != nil {
+		log.Fatal(err)
+	}
+	linkerGrpcServer := grpc.NewServer(
+		grpc.ForceServerCodec(vtcodec.Codec{}),
+		grpc.Creds(insecure.NewCredentials()),
+	)
+
+	pb.RegisterLinkerServer(linkerGrpcServer, linker)
+
+	providerGrpcServer := grpc.NewServer(
+		grpc.ForceServerCodec(vtcodec.Codec{}),
+		grpc.Creds(insecure.NewCredentials()),
+	)
+
+	pb.RegisterProviderServer(linkerGrpcServer, provider)
+
+	// linker
+	go func() {
+		etcdAdd(cli, "/services/linker", "127.0.0.1:5000", 10)
+		log.Println("linker grpc server start")
+		if err := linkerGrpcServer.Serve(linkerLis); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	// provider
+	go func() {
+		etcdAdd(cli, "/services/provider", "127.0.0.1:5001", 10)
+		log.Println("provider grpc server start")
+		if err := providerGrpcServer.Serve(providerLis); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	wc := cli.Watch(context.Background(), "/services/", clientv3.WithPrefix(), clientv3.WithPrevKV())
+	// etcd watcher for client
+	go func() {
+		for wr := range wc {
+			for _, ev := range wr.Events {
+				switch ev.Type {
+				case mvccpb.PUT:
+					switch string(ev.Kv.Key) {
+					case "/services/provider":
+						newAU(string(ev.Kv.Value))
+
+					}
+
+				case mvccpb.DELETE:
+
+				}
+			}
+		}
+	}()
+
+}
+
+type AU struct {
+	pb.ProviderClient
+}
+
+func newAU(addr string) *AU {
+	conn, err := grpc.NewClient(
+		addr,
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithDefaultCallOptions(grpc.ForceCodec(vtcodec.Codec{})),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	client := pb.NewLinkerClient(conn)
+
+	return &AU{client}
+}
+
+func etcdAdd(cli *clientv3.Client, key, val string, ttl int64) {
+	leaseResp, err := cli.Grant(context.Background(), ttl)
+	if err != nil {
+		log.Fatalf("Failed to create lease: %v", err)
+	}
+	_, err = cli.Put(context.Background(), key, val, clientv3.WithLease(leaseResp.ID))
+	if err != nil {
+		log.Fatalf("Failed to put lease: %v", err)
+	}
+	ch, err := cli.KeepAlive(context.Background(), leaseResp.ID)
+	if err != nil {
+		log.Fatalf("etcd keepalive failed: %v", err)
+	}
+	go func() {
+		for ka := range ch {
+			if ka == nil {
+				log.Printf("etcd lease %d expired", leaseResp.ID)
+				return
+			}
+		}
+	}()
 }
